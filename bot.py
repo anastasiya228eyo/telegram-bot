df45ff5eed53d64dd12181098017c58466bdfaa4
anastasiya228eyo <nasty.garnyk@gmail.com>
2026-02-13 13:10:51 +0200

    added weather command

--- bot.py
+++ bot.py
@@ -30,19 +30,19 @@ async def chat(message: types.Message):
         await message.reply("пизда")
 
     if text.startswith("погода "):
-    city = text.replace("погода ", "", 1)
+        city = text.replace("погода ", "", 1)
 
-    try:
-        async with python_weather.Client() as client:
+        try:
+            async with python_weather.Client() as client:
             weather = await client.get(city)
 
-        temp = weather.temperature
-        desc = weather.description
+            temp = weather.temperature
+            desc = weather.description
 
-        await message.reply(f"Сейчас в городе {city.title()} {desc}, {temp}°C")
+             await message.reply(f"Сейчас в городе {city.title()} {desc}, {temp}°C")
 
-    except:
-        await message.reply("не смог найти такой город")
+         except:
+             await message.reply("не смог найти такой город")
 
 async def main():
    await dp.start_polling(bot)   
